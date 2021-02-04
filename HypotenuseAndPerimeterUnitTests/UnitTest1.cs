78b98fcfed981904a9bf9823945560dc2b1cf6d6
Oleksandr Brukhalskyi <bruhalskyi.o@gmail.com>
2021-02-04 14:02:26 +0200

    feat: add parametrized tests for hypotenuse and perimeter tests.

--- HypotenuseAndPerimeterUnitTests/UnitTest1.cs
+++ HypotenuseAndPerimeterUnitTests/UnitTest1.cs
@@ -6,21 +6,23 @@ namespace HypotenuseAndPerimeterUnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void getHypotenuseTest()
-        {
-            double a = 2;
-            double b = 3;
-            double res = TPSiK_lab_1.Program.getHypotenuse(a, b);
-            Assert.AreEqual(res, 3.61);
+        [DataRow(2,3,3.61)]
+        [DataRow(1,1,1.41)]
+        [DataRow(2,1,2.24)]
+        [DataRow(5,4,6.4)]
+        public void getHypotenuseTest(double a, double b, double res)
+        {   
+            double actual = TPSiK_lab_1.Program.getHypotenuse(a, b);
+            Assert.AreEqual(res, actual);
         }
         [TestMethod]
-        public void getPerimeterTest()
+        [DataRow(2,2,2,6)]
+        [DataRow(1,5,3,9)]
+        [DataRow(11,23,10,44)]
+        public void getPerimeterTest(double a, double b, double c, double res)
         {
-            double a = 2;
-            double b = 3;
-            double c = 2;
-            double res = TPSiK_lab_1.Program.getPerimeter(a, b, c);
-            Assert.AreEqual(res, 7);
+            double actual = TPSiK_lab_1.Program.getPerimeter(a, b, c);
+            Assert.AreEqual(res, actual);
         }
     }
 }
